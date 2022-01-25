9d10ccfbfc1e34f2ebcba80beb4d72ad2aea08c9
Liz Larkman <liz@everflowtech.co.uk>
2022-01-25 13:40:13 +0000

    Additional Class and Interface added

---
+++ BattleshipsNS/ShipRoster.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipsNS
+{
+    class ShipRoster : IShipRoster
+    {
+        // Fields, Properties
+        public Ship[] Roster { get; set; }
+
+        // Constructor Declaration of Class
+        public ShipRoster()
+        { }
+
+        // Methods, Events, Operators
+        public void SetShipRoster(ShipType[] roster)
+        { }
+
+        public bool CheckRosterSunk()
+        { return true; }
+
+    }
+}
