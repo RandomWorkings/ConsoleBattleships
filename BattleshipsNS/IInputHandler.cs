95d7da711e54070bf7293643ece487a9ea7d43f4
Liz Larkman <liz@everflowtech.co.uk>
2022-01-25 12:48:57 +0000

    Removed reference comments

--- BattleshipsNS/IInputHandler.cs
+++ BattleshipsNS/IInputHandler.cs
@@ -7,18 +7,7 @@ using System.Threading.Tasks;
 namespace BattleshipsNS
 {
     interface IInputHandler
-    {
-        /*
-         * Responsibility: Management of User Input Validation.
-         * 
-         * The Input Handler should have (Fields or Properties):
-         *  •	No fields or properties.
-         *  
-         *  The system should be able to (Methods, Events, Operators):
-         *  •	Check an input is valid
-         *      o	Returns Boolean.
-         *      
-         */
+    {        
         bool ValidateInput();
     }
 }
--- BattleshipsNS/IOutputHandler.cs
+++ BattleshipsNS/IOutputHandler.cs
@@ -8,19 +8,6 @@ namespace BattleshipsNS
 {
     interface IOutputHandler
     {
-        /*
-         * Responsibility: Management of User Interface (messages and console display).
-         * 
-         * The Output Handler should have (Fields or Properties):
-         *  •	No fields or properties.
-         *  
-         *  The system should be able to (Methods, Events, Operators):
-         *  •	Display appropriate messages
-         *      o	Input integer.
-         *      o	Return void.
-         *      
-         */
         void GenerateMessage(int MessageID);
-
     }
 }
