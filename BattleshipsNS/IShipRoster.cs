f06cf8cbac4de129d003c2d8ec55041988b6b156
Liz Larkman <liz@everflowtech.co.uk>
2022-01-25 13:47:45 +0000

    Actually removed comments

--- BattleshipsNS/IShipRoster.cs
+++ BattleshipsNS/IShipRoster.cs
@@ -8,21 +8,6 @@ namespace BattleshipsNS
 {
     interface IShipRoster
     {
-        /*
-         * 
-         * Responsibility: Management of ship roster class.
-         * 
-         * A ship roster should have (Fields or Properties):
-         *  •	An array of ships
-         *  
-         * The system should be able to (Methods, Events, Operators):
-         *  •	Set a ship roster
-         *      o	Input array of ship types
-         *      o	Return void.
-         *  •	Check if all ships are sunk.
-         *      o	Return Boolean.
-         *      
-         */
         Ship[] Roster { get; set; }
 
         void SetShipRoster(Enum[] roster);
