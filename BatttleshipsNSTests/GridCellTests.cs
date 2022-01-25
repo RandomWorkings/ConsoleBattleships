4e498e906f623e8131c6426bc5df2da8c33927fe
Liz Larkman <liz@everflowtech.co.uk>
2022-01-25 18:39:24 +0000

    GridCellTests - Contents Set Test added

--- BatttleshipsNSTests/GridCellTests.cs
+++ BatttleshipsNSTests/GridCellTests.cs
@@ -11,20 +11,31 @@ namespace BatttleshipsNSTests
         public void GridCell_WhenCalledWithValidInputs_ShouldSetIDAndContents()
         {
             //Arrange
-            int testRow = 1;
-            char testColumn = 'A';
             string expectedID = "A1";
             char expectedContents = ' ';
+            GridCell TestCell = new GridCell(1, 'A');
 
             //Act
-            GridCell TestCell = new GridCell(testRow, testColumn);
-                       
-            //Assess
             string actualID = TestCell.ID;
-            Assert.AreEqual(expectedID, actualID, true , "GridCells Test - ID incorrecly set");
-            
             char actualContents = TestCell.Contents;
-            Assert.AreEqual(expectedContents, actualContents, "GridCells Test - Contents incorrecly set");
+
+            //Assess
+            Assert.AreEqual(expectedID, actualID, true , "GridCellTests - GridCell - ID incorrecly set");          
+            Assert.AreEqual(expectedContents, actualContents, "GridCellTests - GridCell - Contents incorrecly set");
+        }
+        [TestMethod]
+        public void Contents_WhenInputIsValid_ShouldSet()
+        {
+            //Arrange
+            char expectedContents = 'o';
+            GridCell TestCell = new GridCell(1, 'A');
+
+            //Act
+            TestCell.Contents = 'o';
+
+            //Assess
+            char actualContents = TestCell.Contents;
+            Assert.AreEqual(expectedContents, actualContents, "GridCellTests - Contents - Contents incorrecly set");
         }
     }
 }
