ab8c5f903073a207170e6c3adc00c1c700b3c59f
Liz Larkman <liz@everflowtech.co.uk>
2022-01-26 01:31:15 +0000

    replaced method call in program main

--- BattleshipsNS/Program.cs
+++ BattleshipsNS/Program.cs
@@ -24,7 +24,7 @@ namespace BattleshipsNS
 
             foreach (Ship ship in GameParts.Roster)
             {
-                
+                ship.PlaceShip(GameBoard);
             }
 
             // Game Play
