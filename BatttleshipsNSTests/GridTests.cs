ad975965f9a1198dda855d416c2784bcff11184d
Liz Larkman <liz@everflowtech.co.uk>
2022-01-26 15:57:10 +0000

    GridTests cleaned

--- BatttleshipsNSTests/GridTests.cs
+++ BatttleshipsNSTests/GridTests.cs
@@ -7,48 +7,41 @@ namespace BatttleshipsNSTests
     [TestClass]
     public class GridTests
     {
-        //Common Test Variables
-        private readonly int testSize = 100;
+        //Shared Test Inputs
+        private static readonly int testSize = 10;
 
         [TestMethod]
         public void Size_WhenCalled_WithoutArguments_ShouldGetSize()
         {
-            //Arrange
             int expected = testSize;
             Grid TestGrid = new Grid(testSize);
 
-            //Act
             int actual = TestGrid.Size;
 
-            //Assess
-            Assert.AreEqual(expected, actual, ($@"GridTests_Size : Incorrect Grid Size Returned : actual [{actual}], expected [{expected}]"));
+            Assert.AreEqual(expected, actual, ($@"GridTests_Size : Incorrect Grid Size Returned"));
         }
 
         [TestMethod]
 
         public void PlayGrid_WhenCalled_WithValidArguments_ShouldGet2DArrayOfGridCells()
         {
-            //Arrange
             int expectedRank = 2;
             int expectedRowCount = testSize;
             int expectedColCount = testSize;
             Grid TestGrid = new Grid(testSize);
 
-            //Act
             int actualRank = TestGrid.PlayGrid.Rank;
             int actualRowCount = TestGrid.PlayGrid.GetLength(0);
             int actualColCount = TestGrid.PlayGrid.GetLength(actualRank - 1);
 
-            //Assess
-            Assert.AreEqual(expectedRank, actualRank, ($@"GridTests_Grid : Incorrect Multi Dimensional Array Rank Returned : actual [{actualRank}], expected [{expectedRank}"));
-            Assert.AreEqual(expectedRowCount, actualRowCount, ($@"GridTests_Grid : Incorrect Multi Dimensional Array Rows Count Returned : actual [{actualRowCount}], expected [{expectedRowCount}]"));
-            Assert.AreEqual(expectedColCount, actualColCount, ($@"GridTests_Grid : Incorrect Multi Dimensional Array Columns Count Returned : actual [{actualColCount}], expected [{expectedColCount}]"));
-
+            Assert.AreEqual(expectedRank, actualRank, ($@"GridTests_PlayGrid : Incorrect 2-Dimensional Array rank returned"));
+            Assert.AreEqual(expectedRowCount, actualRowCount, ($@"GridTests_PlayGrid : Incorrect 2-Dimensional Array rows count"));
+            Assert.AreEqual(expectedColCount, actualColCount, ($@"GridTests_PlayGrid : Incorrect 2-Dimensional Array columns count"));
             for (int row = 0; row < testSize; row++)
             {
                 for (int col = 0; col < testSize; col++)
                 {
-                    Assert.IsInstanceOfType((TestGrid.PlayGrid[row,col]), typeof(GridCell), ($@"GridTests_Grid: Incorrect Array Contents Type Returned"));
+                    Assert.IsInstanceOfType((TestGrid.PlayGrid[row,col]), typeof(GridCell), ($@"GridTests_PlayGrid: Incorrect 2-Dimensional Array Contents Type returned"));
                 }
             }
         }
