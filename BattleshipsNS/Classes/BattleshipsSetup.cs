0ad2daca5d63d66bab823775b146632382ebba08
Liz Larkman <liz@everflowtech.co.uk>
2022-02-09 12:34:43 +0000

    Made Setup Overload Constructor for testing purposes.

--- BattleshipsNS/Classes/BattleshipsSetup.cs
+++ BattleshipsNS/Classes/BattleshipsSetup.cs
@@ -4,16 +4,34 @@
     {
         public GameParts GameParts { get; private set; }
         public GameBoard GameBoard { get; private set; }
+        private int BoardSize;
+        private ShipTypes[] ShipsList;
 
         public BattleshipsSetup()
         {
             // Program Default settings
-            int boardSize = 10;
-            ShipTypes[] shipsList = { ShipTypes.Battleship, ShipTypes.Destroyer, ShipTypes.Destroyer};
+            BoardSize = 10;
+            ShipTypes[] ShipsList = { ShipTypes.Battleship, ShipTypes.Destroyer, ShipTypes.Destroyer };
 
             // Create Game Components
-            GameParts = new GameParts(shipsList);
-            GameBoard = new GameBoard(boardSize);
+            GameParts = new GameParts(ShipsList);
+            GameBoard = new GameBoard(BoardSize);
+
+            // Setup Game Components
+            foreach (Ship ship in GameParts.Ships)
+            {
+                ship.PlaceShip(GameBoard);
+            }
+        }
+
+        public BattleshipsSetup(int boardSize, ShipTypes[] shipsList)
+        {
+            BoardSize = boardSize;
+            ShipsList = shipsList;
+
+            // Create Game Components
+            GameParts = new GameParts(ShipsList);
+            GameBoard = new GameBoard(BoardSize);
 
             // Setup Game Components
             foreach (Ship ship in GameParts.Ships)
