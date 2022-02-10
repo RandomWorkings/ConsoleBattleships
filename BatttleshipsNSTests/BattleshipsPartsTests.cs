082cf69fbaf306083ff6749f1f1adb232ad60432
Liz Larkman <liz@everflowtech.co.uk>
2022-02-10 16:04:40 +0000

    Refactored BattleshipPartsTests

--- BatttleshipsNSTests/BattleshipsPartsTests.cs
+++ BatttleshipsNSTests/BattleshipsPartsTests.cs
@@ -7,61 +7,86 @@ namespace BattleshipsNSTests
     public class BattleshipsPartsTests
     {
         [TestMethod]
-        public void GameParts_WhenCalled_WithValidArguments_SetShipCount()
+        public void BattleshipsParts_WhenCalled_GivenNoArguments_ThenSetDefaultShipCount()
         {
-            int expected = 1;
-            ShipTypes[] testShipsList = { ShipTypes.Destroyer };
-            BattleshipsParts testParts = new BattleshipsParts(testShipsList);
+            int expected = 3;
+            BattleshipsParts testParts = new BattleshipsParts();
 
             int actual = testParts.ShipCount;
 
-            Assert.AreEqual(expected, actual, ($@"GamePartsTests_SetShipCount : Incorrect Ship Count value returned"));
+            Assert.AreEqual(expected, actual, ($@"BattleshipsPartsTests_SetShipCount : Incorrect Ship Count value returned"));
         }
 
         [TestMethod]
-        public void GameParts_WhenCalled_WithValidArguments_SetShipArrayLength()
+        public void BattleshipsParts_WhenCalled_GivenValidArguments_ThenSetCorrectShipCount()
         {
             int expected = 1;
             ShipTypes[] testShipsList = { ShipTypes.Destroyer };
-            BattleshipsParts testParts = new BattleshipsParts(testShipsList);
+            IBattleshipsParts testParts = new BattleshipsParts(testShipsList);
+
+            int actual = testParts.ShipCount;
+
+            Assert.AreEqual(expected, actual, ($@"BattleshipsPartsTests_SetShipCount : Incorrect Ship Count value returned"));
+        }
+        [TestMethod]
+        public void BattleshipsParts_WhenCalled_GivenValidArguments_ThenSetDefaultShipArrayLength()
+        {
+            int expected = 3;
+            IBattleshipsParts testParts = new BattleshipsParts();
 
             int actual = testParts.Ships.Length;
 
-            Assert.AreEqual(expected, actual, ($@"GamePartsTests_SetShipArrayLength : Incorrect Ship Array Length value returned"));
+            Assert.AreEqual(expected, actual, ($@"BattleshipsPartsTests_SetShipArrayLength : Incorrect Ship Array Length value returned"));
         }
-
         [TestMethod]
-        public void GameParts_WhenCalled_WithValidArguments_SetArrayContents()
-        {
-            ShipTypes[] testShipsList = { ShipTypes.Destroyer };
-            BattleshipsParts testParts = new BattleshipsParts(testShipsList);
-
-            Ship actual = testParts.Ships[0];
-
-            Assert.IsNotNull(actual, $@"GamePartsTests_SetArrayContent : No Array Contents exists");
-        }
-
-        [TestMethod]
-        public void UpdateShipCount_WhenCalled_AndNoNewShipSunk_DoNothing()
+        public void BattleshipsParts_WhenCalled_GivenValidArguments_ThenSetCorrectShipArrayLength()
         {
             int expected = 1;
             ShipTypes[] testShipsList = { ShipTypes.Destroyer };
-            BattleshipsParts testParts = new BattleshipsParts(testShipsList);
+            IBattleshipsParts testParts = new BattleshipsParts(testShipsList);
+
+            int actual = testParts.Ships.Length;
+
+            Assert.AreEqual(expected, actual, ($@"BattleshipsPartsTests_SetShipArrayLength : Incorrect Ship Array Length value returned"));
+        }
+        [TestMethod]
+        public void BattleshipsParts_WhenCalled_GivenNo_ThenSetDefaultArrayContents()
+        {
+           IBattleshipsParts testParts = new BattleshipsParts();
+
+            IShip actual = testParts.Ships[0];
+
+            Assert.IsNotNull(actual, $@"BattleshipsPartsTests_SetArrayContent : No Array Contents exists");
+        }
+        [TestMethod]
+        public void BattleshipsParts_WhenCalled_GivenValidArguments_ThenSetCorrectArrayContents()
+        {
+            ShipTypes[] testShipsList = { ShipTypes.Destroyer };
+            IBattleshipsParts testParts = new BattleshipsParts(testShipsList);
+
+            IShip actual = testParts.Ships[0];
+
+            Assert.IsNotNull(actual, $@"BattleshipsPartsTests_SetArrayContent : No Array Contents exists");
+        }
+        [TestMethod]
+        public void BattleshipsParts_UpdateShipCount_WhenCalled_GivenNoShipSunk_ThenReturnCorrectValue()
+        {
+            int expected = 1;
+            ShipTypes[] testShipsList = { ShipTypes.Destroyer };
+            IBattleshipsParts testParts = new BattleshipsParts(testShipsList);
             testParts.UpdateShipCount();
 
             int actual = testParts.ShipCount;
 
-            Assert.AreEqual(expected, actual, ($@"GamePartsTests_UpdateShipCount : Incorrect Ship Count value returned"));
+            Assert.AreEqual(expected, actual, ($@"BattleshipsPartsTests_UpdateShipCount : Incorrect Ship Count value returned"));
         }
-
         [TestMethod]
-        public void UpdateShipCount_WhenCalled_AndANewShipSunk_ModifiyShipCount()
+        public void BattleshipsParts_UpdateShipCount_WhenCalled_GivenAShipSunk_ThenReturnCorrectValue()
         {
             int expected = 0;
-            Ship testShip = new Ship(ShipTypes.Destroyer);
             ShipTypes[] testShipsList = { ShipTypes.Destroyer };
-            BattleshipsParts testParts = new BattleshipsParts(testShipsList);
-            for (int i = 0; i < testShip.Length; i++)
+            IBattleshipsParts testParts = new BattleshipsParts(testShipsList);
+            for (int i = 0; i < testParts.Ships[0].Length; i++)
             {
                 testParts.Ships[0].Sections[i].Contents = 'x';
             }
@@ -69,55 +94,7 @@ namespace BattleshipsNSTests
 
             int actual = testParts.ShipCount;
 
-            Assert.AreEqual(expected, actual, ($@"GamePartsTests_UpdateShipCount : Incorrect Ship Count value returned"));
+            Assert.AreEqual(expected, actual, ($@"BattleshipsPartsTests_UpdateShipCount : Incorrect Ship Count value returned"));
         }
-        [TestMethod]
-        public void UpdateSunkFlag_WhenCalled_AndShipNotSunk_AndShipSectionsNotAllHit_DoNothing()
-        {
-            bool expected = false;
-            ShipTypes testShipType = ShipTypes.Battleship;
-            Ship testShip = new Ship(testShipType);
-            testShip.UpdateSunk();
-
-            bool actual = testShip.Sunk;
-
-            Assert.AreEqual(expected, actual, ($@"ShipTests_UpdateSunkFlag_NotSunk : Returned incorrect computed value."));
-        }
-
-        [TestMethod]
-        public void UpdateSunkFlag_WhenCalled_AndShipNotSunk_AndShipSectionsAllHit_SetSunkFlag_True()
-        {
-            bool expected = true;
-            ShipTypes testShipType = ShipTypes.Battleship;
-            Ship testShip = new Ship(testShipType);
-            foreach (BoardSpace section in testShip.Sections)
-            {
-                section.Contents = 'x';
-            }
-            testShip.UpdateSunk();
-
-            bool actual = testShip.Sunk;
-
-            Assert.AreEqual(expected, actual, ($@"ShipTests_UpdateSunkFlag_Sunk : Returned computed set value."));
-        }
-
-        [TestMethod]
-        public void UpdateSunkFlag_WhenCalled_AndShipSunk_DoNothing()
-        {
-            bool expected = true;
-            ShipTypes testShipType = ShipTypes.Battleship;
-            Ship testShip = new Ship(testShipType);
-            foreach (BoardSpace section in testShip.Sections)
-            {
-                section.Contents = 'x';
-            }
-            testShip.UpdateSunk(); // Sets up a sunk flag
-            testShip.UpdateSunk(); // Method with already sunk ship
-
-            bool actual = testShip.Sunk;
-
-            Assert.AreEqual(expected, actual, ($@"ShipTests_UpdateSunkFlag_Sunk : Returned computed set value."));
-        }
-
     }
 }
